e6362eb0aa76893b30adca2cbb6b4a402a168dc1
iNailuY <inailuy@me.com>
2016-03-18 19:12:34 -0500

    progress..

--- MemeMe/MemeMe/EditorVC.swift
+++ MemeMe/MemeMe/EditorVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class EditorVC: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -15,69 +16,74 @@ class EditorVC: UIViewController, UITextFieldDelegate, UIImagePickerControllerDe
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     var imagePicker = UIImagePickerController()
-    var height : CGFloat!
-    var contentInsets : UIEdgeInsets!
+    var bottomFrame: CGRect!
+    var isEditingBottomFrame :Bool!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        height = 0
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        bottomFrame = bottomTextfield.frame
+        isEditingBottomFrame = false
+        
+        self.drawTextInRect(topTextfield)
+        self.drawTextInRect(bottomTextfield)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // Keyboard stuff.
+        let center: NSNotificationCenter = NSNotificationCenter.defaultCenter()
+        center.addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        center.addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        NSNotificationCenter.defaultCenter().removeObserver(self)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
     }
+    
     
     func replaceTextFieldFrames(){
         let frame = bottomTextfield.frame
         bottomTextfield.frame = topTextfield.frame
         topTextfield.frame = frame
     }
+    
     func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
-            if keyboardSize.height > 0 {
-                let contentInsets: UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height, 0.0)
-                scrollView.contentInset = contentInsets
-                scrollView.scrollIndicatorInsets = contentInsets
-                
-                var aRect: CGRect = self.view.frame
-                aRect.size.height -= keyboardSize.height
-                let activeTextFieldRect: CGRect? = bottomTextfield?.frame
-                let activeTextFieldOrigin: CGPoint? = activeTextFieldRect?.origin
-                if (!CGRectContainsPoint(aRect, activeTextFieldOrigin!)) {
-                    scrollView.scrollRectToVisible(activeTextFieldRect!, animated:true)
-                }
-            }
+        if isEditingBottomFrame == true {
+            UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+                self.bottomTextfield.frame = self.topTextfield.frame
+                self.topTextfield.hidden = true
+                }, completion: nil)
         }
     }
     
     func keyboardWillHide(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
-            if keyboardSize.height > 0 {
-                let contentInsets: UIEdgeInsets = UIEdgeInsetsZero
-                scrollView.contentInset = contentInsets
-                scrollView.scrollIndicatorInsets = contentInsets
-            }
+        if isEditingBottomFrame == true {
+            UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+                self.bottomTextfield.frame = self.bottomFrame
+                self.topTextfield.hidden = false
+                }, completion: nil)
         }
-    }
-    @IBAction func cancelButtonPressed(sender: AnyObject) {
-        view.endEditing(true)
     }
 
     func textFieldDidBeginEditing(textField: UITextField) {
         if textField == bottomTextfield {
-            scrollView.scrollEnabled = true
+            isEditingBottomFrame = true
+        }else {
+            isEditingBottomFrame = false
         }
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
-        if textField == bottomTextfield {
-            scrollView.scrollEnabled = true
-        }
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
     }
     
     func textFieldShouldEndEditing(textField: UITextField) -> Bool {
@@ -118,6 +124,11 @@ class EditorVC: UIViewController, UITextFieldDelegate, UIImagePickerControllerDe
         }
         
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func drawTextInRect(textField: UITextField) {
+        let attributeDictionary = [NSStrokeWidthAttributeName: -4.0, NSStrokeColorAttributeName : UIColor.blackColor(), NSForegroundColorAttributeName : UIColor.whiteColor()]
+        textField.attributedText = NSAttributedString(string: (textField.text)!, attributes: attributeDictionary)
     }
 }
 
